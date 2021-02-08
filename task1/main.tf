b2b44bfcba064c7cf78c1679bd2fcf86bfbf6497
Ubuntu <micha@terraformvm.ngd3ji2322huvh442gbrnbrjee.zx.internal.cloudapp.net>
2021-02-08 16:29:36 +0000

    task1

---
+++ task1/main.tf
@@ -0,0 +1,123 @@
+// Terraform Config Block
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+// Provider
+provider "azurerm" {
+  features {}
+}
+
+// Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "${var.pn}resources"
+  location = var.location
+}
+
+
+// Virtual Network
+resource "azurerm_virtual_network" "example" {
+  name                = "${var.pn}network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+// Subnet
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+// Public IP
+resource "azurerm_public_ip" "example" {
+  name                = "acceptanceTestPublicIp1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+// Network Security Group
+resource "azurerm_network_security_group" "example" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+// Network Interface/NSG Group Association
+
+resource "azurerm_network_interface" "example" {
+  name                = "${var.pn}-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+
+
+// Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "${var.pn}-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = var.vm_size
+  admin_username      = "micha"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = "micha"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
---
+++ task1/variables.tf
@@ -0,0 +1,14 @@
+// Location Name Variable
+variable "location" {
+    default = "UK South"
+}
+
+// Project Name
+variable "pn" {
+    default = "example"
+}
+
+// VM Size
+variable "vm_size" {
+    default = "Standard_B1ms"
+}
